73247009b951b4fe9c0bd1446f2fc60888cbd780
sainath199 <39979877+sainath199@users.noreply.github.com>
2018-09-10 14:08:48 +0530

    Update cookies.py
    MFP-1

--- import requests url_0 - -https/jira-uat.us.oracle.com/jira- url - -https/jira-uat.us.oracle.com/cookies.py
+++ import requests url_0 - -https/jira-uat.us.oracle.com/jira- url - -https/jira-uat.us.oracle.com/cookies.py
@@ -2,7 +2,7 @@ import requests
 url_0 = "https://jira-uat.us.oracle.com/jira"
 url = "https://jira-uat.us.oracle.com/jira"
 data = {"username": , "password": "*****"}
-
+testing for first time
 s = requests.session()
 s.get(url_0)
 r = s.post(url, data)
